4c5aba0eca76a626e9b35227b12d8032842efe05
Thomas Röggla <tom@lichtfragmente.com>
2024-02-16 04:41:58 +0100

    constructor for OSCEndpoint now takes reference to socket

--- musicwand_esp32/osc_endpoint.hpp
+++ musicwand_esp32/osc_endpoint.hpp
@@ -8,13 +8,13 @@
 #define LOCAL_PORT 2390
 
 class OSCEndpoint {
-  WiFiUDP socket;
+  WiFiUDP& socket;
   IPAddress remoteAddress;
   uint16_t remotePort;
 
 public:
-  OSCEndpoint(WiFiUDP socket, uint16_t remotePort) : OSCEndpoint(socket, IPAddress(255, 255, 255, 255), remotePort) {}
-  OSCEndpoint(WiFiUDP socket, IPAddress remoteAddress, uint8_t remotePort) : socket(socket), remoteAddress(remoteAddress), remotePort(remotePort) {}
+  OSCEndpoint(WiFiUDP& socket, uint16_t remotePort) : OSCEndpoint(socket, IPAddress(255, 255, 255, 255), remotePort) {}
+  OSCEndpoint(WiFiUDP& socket, IPAddress remoteAddress, uint8_t remotePort) : socket(socket), remoteAddress(remoteAddress), remotePort(remotePort) {}
 
   void sendMessage(const char route[], int32_t value);
 };
